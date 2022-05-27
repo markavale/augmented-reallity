715144c68b2000dbf9aceeb7ff1785d115f9d0a6
Mark Anthony Vale <markavale@pop-os.localdomain>
2022-05-27 16:25:39 +0800

    updated

--- augmented.py
+++ augmented.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(1)
 imgTarget = cv2.imread('target.jpg')
 myVid = cv2.VideoCapture('osaka-vid.mp4')
 print(myVid.isOpened())
